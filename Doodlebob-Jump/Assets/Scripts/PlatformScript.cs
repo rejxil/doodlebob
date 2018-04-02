bc2aa6e40fd0cffb61a3e554a64c854b90a68d36
rejxil <36443591+rejxil@users.noreply.github.com>
2018-04-02 16:32:05 +0200

    Horizontal Movement

--- Doodlebob-Jump/Assets/Scripts/PlatformScript.cs
+++ Doodlebob-Jump/Assets/Scripts/PlatformScript.cs
@@ -8,14 +8,21 @@ public class PlatformScript : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D> ();
-		//only allows collider and rigidbody to collide with each other 
-		if (rb != null) {
-			//Velocity will be the result of the force of bouncing upwards
-			//sets speed we want doodle to travel upwards after bouncing
-			Vector2 velocity = rb.velocity;
-			velocity.y = jumpForce; 
-			rb.velocity = velocity;
+		//upward force being applied due to 2D effector but no collision is actually happening
+		//therefore we need to check if object is coming from above or from below
+
+		//this means that we are coming
+		if (collision.relativeVelocity.y <= 0) {
+
+			Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D> ();
+			//only allows collider and rigidbody to collide with each other 
+			if (rb != null) {
+				//Velocity will be the result of the force of bouncing upwards
+				//sets speed we want doodle to travel upwards after bouncing
+				Vector2 velocity = rb.velocity;
+				velocity.y = jumpForce; 
+				rb.velocity = velocity;
+			}
 		}
 	}
 }
---
+++ Doodlebob-Jump/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))] //to ensure that there will always be a Rigidbody component on the gameobject
+
+public class Player : MonoBehaviour {
+
+	public float movementSpeed = 10f;
+
+	//reference to our Rigidbody2D
+	Rigidbody2D rb;
+
+	float movement = 0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = Input.GetAxis("Horizontal") * movementSpeed;
+	}
+
+	void FixedUpdate()
+	{
+		Vector2 velocity = rb.velocity;
+		velocity.x = movement;
+		rb.velocity = velocity;
+	}
+}
