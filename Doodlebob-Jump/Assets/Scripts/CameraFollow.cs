2089d51faded785642e98dd56396ce5ed6af6dc5
rejxil <36443591+rejxil@users.noreply.github.com>
2018-04-02 17:09:50 +0200

    Platform random generation

--- Doodlebob-Jump/Assets/Scripts/CameraFollow.cs
+++ Doodlebob-Jump/Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	public Transform target;
+
 	// Update is called once per frame
 	void Update () {
+
+		//we want to check if our target's y value is greater than the camera's y value. 
+		//camera only follows doodle when he moves upwards
+		if (target.position.y > transform.position.y) {
+
+			Vector3 newPos = new Vector3(0f, target.position.y, -10f);
+			transform.position = newPos;
+			//transform.position = new Vector3 (transform.position.x, target.position.y, target.position.z); //for some reason this doesn't work for me
+
+		}
 		
 	}
 }
