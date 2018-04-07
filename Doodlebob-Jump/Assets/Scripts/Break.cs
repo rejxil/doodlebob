17c265f2aa50aeef1f08f20452692df3d96beb22
rejxil <36443591+rejxil@users.noreply.github.com>
2018-04-07 10:28:03 +0200

    Red pencil change animation, produce no points, and no bounce

--- Doodlebob-Jump/Assets/Scripts/Break.cs
+++ Doodlebob-Jump/Assets/Scripts/Break.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class Break : MonoBehaviour {
 
+	public float jumpForce = 10f; 
 
+	public Sprite platform; 
+	public Sprite brokenPlatform;
 
-	public void OnCollisionEnter2D(Collision2D collision)
+	private SpriteRenderer spriteRenderer;
+
+	void Start() {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		if (spriteRenderer.sprite == null)
+			spriteRenderer.sprite = platform;
+	}
+
+	public void OnCollisionEnter2D(Collision2D coll)
 	{
 
 		//upward force being applied due to 2D effector but no collision is actually happening
 		//therefore we need to check if object is coming from above or from below
 
-		if (collision.relativeVelocity.y <= 0) {
+		if (coll.relativeVelocity.y <= 0) {
 
-			Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D> ();
+			Rigidbody2D rb = coll.collider.GetComponent<Rigidbody2D> ();
 			//only allows collider and rigidbody to collide with each other 
 			if (rb != null) {
-
+				Vector2 velocity = rb.velocity;
+				velocity.y = jumpForce; 
+				rb.velocity = velocity;
+				ChangeSprite ();
+				Destroy (GetComponent<EdgeCollider2D> ());
 			}
-				
-			Destroy (gameObject);
-
 		}
+	}
+
+	void ChangeSprite() {
+		if (spriteRenderer.sprite == platform) {
+			spriteRenderer.sprite = brokenPlatform;
+		} 
 	}
 }
